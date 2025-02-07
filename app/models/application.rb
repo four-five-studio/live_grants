2393b49eba717dbd874dce4093ae6c4f3523f9b5
James Darling <james@abscond.org>
2025-02-07 13:21:07 +0000

    Add multidimensional scoring

--- app/models/application.rb
+++ app/models/application.rb
@@ -16,11 +16,29 @@ class Application < ApplicationRecord
     elegibility == true
   end
 
-  def score
-    score = 0
-    score += 1 if per_person < 100
-    score -= 1 if per_person > 1000
-    score
+  def keen_to_hear_more?
+    good_points.count > bad_points.count
+  end
+
+  def good_points
+    points = []
+    points << "You are asking for an expected amount of money per person" if per_person < 60 && per_person > 10
+    points << "You are working in an area with a high level of deprivation" if imd_decile < 5
+    points << "You are working with demographics that we are particularly interested in" if demographic_target_score > 10
+    points << "We aren't currently funding much for #{primary_demographic} in #{location_name}" unless already_funding?(primary_demographic)
+    points << "We aren't currently funding much for #{sport} in #{location_name}" unless already_funding?(sport)
+    points
+  end
+
+  def bad_points
+    points = []
+    points << "It seems like you are asking for a lot of money per person" if per_person > 100
+    points << "You're asking for less per person than we would expect" if per_person < 10
+    points << "You are working in an area with a low level of deprivation" if imd_decile > 5
+    points << "It doesn't look like you're including groups that we are particularly interested in funding" if demographic_target_score < 10
+    points << "We already fund lots of organisations for #{primary_demographic} in #{location_name}" if already_funding?(primary_demographic)
+    points << "We already fund lots of organisations for #{sport} in #{sport}" if already_funding?(sport)
+    points
   end
 
   def per_person
@@ -66,5 +84,19 @@ class Application < ApplicationRecord
 
   def applications_in_location
     Random.new(location_name.hash).rand(0..10)
+  end
+
+  def already_funding?(name)
+    Random.new(location_name.hash + name.hash).rand(0..1) == 1
+  end
+
+  def primary_demographic
+    demographics = {
+      "People on Low Income" => low_income.to_i,
+      "Children" => children.to_i,
+      "LGBTQ people" => lgbtq.to_i,
+      "Older people" => older.to_i
+    }
+    demographics.max_by { |_, value| value }[0]
   end
 end
